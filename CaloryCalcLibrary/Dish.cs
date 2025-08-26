a117118531385fe54e93cde0bfd3f684d0396b33
Shers_um49 <Shers_um49@student.itstep.org>
2025-08-26 23:02:56 +0300

    Додано 'CaloryCalcLibrary` з класами і змінними як в описі. Колеги, подивіться будь ласка і перевірте на помилки. Дякую дуже.
    Added CaloryCalcLibrary with classes as per description.

---
+++ CaloryCalcLibrary/Dish.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaloryCalcLibrary
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Dish name")]
+        public string Name { get; set; } = default!;
+    }
+}
---
+++ CaloryCalcLibrary/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloryCalcLibrary
+{
+    internal class Product
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Product")]
+        public string Name { get; set; } = default!;
+
+        public float density { get; set; }
+
+        public float calories { get; set; }
+
+        public float fats { get; set; }
+
+        public float carbohydrates { get; set; }
+
+        public float proteins { get; set; }
+
+    }
+}
