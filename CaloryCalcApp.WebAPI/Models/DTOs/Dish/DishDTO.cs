0c7cf8b1814960a9ba1dc84a13ac3f777cdadbfc
Shers_um49 <Shers_um49@student.itstep.org>
2025-09-14 01:14:30 +0300

    Working on DishController

--- CaloryCalcApp.WebAPI/Models/DTOs/Dish/DishDTO.cs
+++ CaloryCalcApp.WebAPI/Models/DTOs/Dish/DishDTO.cs
@@ -1,4 +1,5 @@
-﻿using CaloryCalcLibrary;
+﻿using CaloryCalcApp.WebAPI.Models.DTOs.DishProduct;
+using CaloryCalcLibrary;
 using System.ComponentModel.DataAnnotations;
 
 namespace CaloryCalcApp.WebAPI.Models.DTOs.Dish
@@ -7,5 +8,10 @@ namespace CaloryCalcApp.WebAPI.Models.DTOs.Dish
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
+        public List<DishProductDTO> DishProducts { get; set; } = new();
+        public double TotalFats { get; set; }
+        public double TotalCalories { get; set; }
+        public double TotalProteins { get; set; }
+        public double TotalCarbohydrates { get; set; }
     }
 }
---
+++ CaloryCalcApp.WebAPI/Models/DTOs/HealthyUserDTO/HealthyUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using CaloryCalcApp.WebAPI.Models.DTOs.Dish;
+
+namespace CaloryCalcApp.WebAPI.Models.DTOs.HealthyUserDTO
+{
+    public class HealthyUserDTO
+    {
+        public double Weight { get; set; } = default!; // Вага
+
+        public int Height { get; set; } = default!; // Зріст
+
+        public DateOnly DateOfBirth { get; set; } // Дата народження
+
+        public string Sex { get; set; } = default!;
+
+        public List<DishDTO> Dishes { get; set; } = [];
+    }
+}
