bd6a68f44a438594c0e8bff4f0ef1762fe6679e8
Shers_um49 <Shers_um49@student.itstep.org>
2026-02-05 21:26:03 +0200

    Commented using of Swagger. Home page is in use as default.

--- CaloryCalcApp.WebAPI/Program.cs
+++ CaloryCalcApp.WebAPI/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 string connStr = builder.Configuration.GetConnectionString("CaloriesContext")
     ?? throw new InvalidOperationException("Connection string 'CaloriesContext' not found!");
@@ -28,50 +28,30 @@ builder.Services.AddDbContext<CaloriesContext>(options => {
 builder.Services.AddIdentityApiEndpoints<HealthyUser>()
     .AddEntityFrameworkStores<CaloriesContext>();
 
-//builder.Services.AddAutoMapper(cfg => { }, typeof(ProductProfile),
-//    typeof(DishProfile), typeof(HealthyUserProfile), typeof(HealthyUserDishProfile),
-//    typeof(DishProductProfile));
 
-builder.Services.AddSwaggerGen(options => {
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Name = "Authorization"
-    });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-}
-    );
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen(options => {
+//    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+//    {
+//        In = ParameterLocation.Header,
+//        Type = SecuritySchemeType.ApiKey,
+//        Name = "Authorization"
+//    });
+//    options.OperationFilter<SecurityRequirementsOperationFilter>();
+//    }
+// );
 // Adding Controllers with Views
 builder.Services.AddControllersWithViews();
-// Configuring Identity options
-//builder.Services.AddIdentity<HealthyUser, IdentityRole>(
-//    options =>
-//    {
-//        options.Password.RequiredLength = 8;
-//        options.Password.RequireNonAlphanumeric = false;
-//        options.Password.RequireDigit = true;
-//        options.Password.RequireUppercase = true;
-//        options.Password.RequireLowercase = true;
-
-//    });
-
 
 // Adding AutoMapper
 builder.Services.AddAutoMapper(cfg =>
-{
-    cfg.AddProfile(new ProductProfile());
-    cfg.AddProfile(new DishProductProfile());
-    cfg.AddProfile(new DishProfile());
-    cfg.AddProfile(new HealthyUserDishProfile());
-    cfg.AddProfile(new HealthyUserProfile());
+    {
+        cfg.AddProfile(new ProductProfile());
+        cfg.AddProfile(new DishProductProfile());
+        cfg.AddProfile(new DishProfile());
+        cfg.AddProfile(new HealthyUserDishProfile());
+        cfg.AddProfile(new HealthyUserProfile());
 
-}
-    //typeof(DishProductProfile),
-    //typeof(DishProfile),
-    //typeof(HealthyUserDishProfile),
-    //typeof(HealthyUserProfile),    
+    }   
 );
 var app = builder.Build();
 
@@ -89,17 +69,17 @@ using (IServiceScope scope = app.Services.CreateScope())
 }
 
 
-
 /////////////////////////////////////////
 ///
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    //app.UseExceptionHandler("/Home/Error");
-    //app.UseHsts();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    //app.UseExceptionHandler("/Home/Error");
+//    //app.UseHsts();
+//}
+
 app.MapIdentityApi<HealthyUser>();
 app.UseHttpsRedirection();
 
@@ -113,6 +93,6 @@ app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     //pattern: "{controller=Home}/{action=Index}/{id?}");
-    pattern: "{controller=Account}/{action=Register}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
