03ed8b658de3b81dec9e3803ff77493c05aa6da9
Shers_um49 <Shers_um49@student.itstep.org>
2026-02-11 21:17:59 +0200

    Working full power on the search. For the moment created search for Products, Dishes and Users (all by Name). Remaining to make nice tables with buttons to go to details, delete and edit for Dishes and Users (if Admin). Remaining to make pagination for all items. Need to make more beautiful looking of search into Layout.

---
+++ CaloryCalcApp.WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CaloryCalcApp.WebAPI.Data;
+using CaloryCalcApp.WebAPI.Models.ViewModels.Search;
+using CaloryCalcLibrary;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering; // <-- for SelectListItem
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CaloryCalcApp.WebAPI.Controllers
+{
+    [Authorize]
+    public class SearchController : Controller
+    {
+        private readonly CaloriesContext _context;
+        private readonly IMapper _mapper;
+        private readonly UserManager<HealthyUser> _userManager;
+
+        public SearchController(CaloriesContext context, IMapper mapper, UserManager<HealthyUser> userManager)
+        {
+            _context = context;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        // GET: Search
+        [HttpGet]
+        public IActionResult Index()
+        {            
+            return View("_Search");
+        }
+
+        // POST: Search
+        [HttpPost]
+        public async Task<ActionResult> Search(SearchViewModel model)
+        {
+            // Let's check if our `SearchText` is not empty
+            if (model != null)
+            {
+                if (model.SearchText != null && model.SearchType != null)
+                {
+                    if(model.SearchType == "Product" || model.SearchType == "FullSearch")
+                    {
+                        // Let's choouse products where `Name` contains symbols from our search
+                        model.ProductsFound = await _context.Products.Where(p => p.Name.Contains(model.SearchText)).OrderBy(p => p.Id).ToListAsync();
+                    }
+                    if (model.SearchType == "Dish" || model.SearchType == "FullSearch")
+                    {
+                        // Let's choouse dishes where `Name` contains symbols from our search
+                        model.DishesFound = await _context.Dishes.Where(p => p.Name.Contains(model.SearchText)).OrderBy(p => p.Id).ToListAsync();
+                    }
+                    if (model.SearchType == "User" || model.SearchType == "FullSearch")
+                    {
+                        // Let's choouse products where `Name` contains symbols from our search
+                        model.UsersFound = await _userManager.Users.Where(p => p.UserName.Contains(model.SearchText)).OrderBy(p => p.Id).ToListAsync();
+                    }
+                }
+            }
+
+            // Example: redirect to a results page
+            return View("_Search", model);
+        }
+    }
+}
---
+++ CaloryCalcApp.WebAPI/Models/ViewModels/Search/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CaloryCalcLibrary;
+using Microsoft.AspNetCore.Mvc.Rendering; // for SelectListItem
+using System.Collections.Generic;
+
+namespace CaloryCalcApp.WebAPI.Models.ViewModels.Search
+{
+    public class SearchViewModel
+    {
+        public string SearchText { get; set; } = string.Empty;
+        public string SearchType { get; set; } = string.Empty;
+        public List<Product> ProductsFound { get; set; } = new List<Product>();
+        public List<Dish> DishesFound { get; set; } = new List<Dish>();
+        public List<HealthyUser> UsersFound { get; set; } = new List<HealthyUser>();
+    }
+}
